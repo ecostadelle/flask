bdac467451f48f6cc6aedd1cc786c07f6d8c48d9
Ewerton Costadelle <ecostadelle@id.uff.br>
2022-10-04 23:10:09 -0400

    Estrura de pastas

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-
-def home():
-    return "Olá mundo!"
-
-@app.route('/<val>')
-
-def previsao(val):
-    return val
-
-app.run(port=80)
+from app import app
